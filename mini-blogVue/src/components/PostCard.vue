75521c0904d566b9a2c01f77b69ea3c8f278cece
DelcroixFlorian <florian.delcroix98@gmail.com>
2021-01-03 23:01:06 +0100

    home page
    +postCard

--- mini-blogVue/src/components/PostCard.vue
+++ mini-blogVue/src/components/PostCard.vue
@@ -1,13 +1,26 @@
 <template>
-  
+    <div class="containerPost">
+        <div class="postTitle">
+            <h1>{{title}}</h1>
+        </div>
+        <div class="author">
+            <p>{{author}}</p>
+        </div>
+        <a class="see" href="/"><i>See more...</i></a>
+    </div>
 </template>
 
 <script>
 export default {
+    name: "PostCard",
+    props: {
+        title: String,
+        author: String
+    }
 
 }
 </script>
 
 <style>
-
+    @import "../styles/postCard.css";
 </style>
--- mini-blogVue/src/views/Home.vue
+++ mini-blogVue/src/views/Home.vue
@@ -1,12 +1,52 @@
 <template>
-    <div>
-        <p>Bonjour</p>
+    <div class="homeContainer">
+        <h2>Articles</h2>
+        <div class="postContainer">
+          <postCard class="card" v-for="article in articleList" :key="article.title" v-bind:title="article.title" v-bind:author="article.author"></postCard>
+        </div>
     </div>
 </template>
 <script>
+import PostCard from "../components/PostCard.vue"
+
 export default {
-  name: 'Home' 
+  name: 'Home',
+  data () {
+    return {
+      articleList: [
+        {
+          title: "mon incroyable article 1",
+          author: "author 1"
+        },
+        {
+          title: "mon incroyable article 2",
+          author: "author 2"
+        },
+        {
+          title: "mon incroyable article 3",
+          author: "author 3"
+        },
+        {
+          title: "mon incroyable article 4, le plus beau du monde",
+          author: "author 4"
+        },
+        {
+          title: "mon incroyable dernier article",
+          author: "author 3"
+        },
+      ]
+    }
+  },
+  components: {
+    PostCard,
+  },
+  created: {
+
+  }
+
 }
 </script>
+
 <style>
+  @import "../styles/home.css";
 </style>
