3eb8b6677d6adfa98852bf2481fe50912c192653
DelcroixFlorian <florian.delcroix98@gmail.com>
2021-01-06 15:14:55 +0100

    api

--- mini-blogVue/src/components/Login.vue
+++ mini-blogVue/src/components/Login.vue
@@ -1,18 +1,20 @@
 <template>
  <div>
-   <form class="login" @submit.prevent="login">
+   <form class="login" action="log()">
      <h1>Sign in</h1>
      <label>Email</label>
      <input required v-model="userName" type="email" placeholder="Snoopy@gmail.com"/>
      <label>Password</label>
      <input required v-model="password" type="password" placeholder="Password"/>
      <router-link to="/register" class="regis">Register</router-link>
-     <button type="submit">Login</button>
+     <button type="submit" >Login</button>
    </form>
  </div>
 </template>
 
 <script>
+import file from "../API/api"
+
 export default {
   name: "login",
   props: {
@@ -25,12 +27,9 @@ export default {
     }
   },
   methods: {
-    /*login: function () {
-      const { username, password } = this
-      this.$store.dispatch(AUTH_REQUEST, { username, password }).then(() => {
-        this.$router.push('/')
-      })
-    }*/
+    log: function () {
+      file.login()
+    }
   }
 }
 </script>
