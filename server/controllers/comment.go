47219e99bda0a6f782e5767fd7ab917f5d97992e
0aubsq <0aubsq@gmail.com>
2021-01-08 20:01:23 +0100

    CFUD Comments

---
+++ server/controllers/comment.go
@@ -0,0 +1,88 @@
+package controllers
+
+import (
+  "math/rand"
+  u "../utils"
+  m "../models"
+)
+
+func CommentCreate_c(token string, aid string, content string) int {
+  // Check if user is logged
+  uid, is_user := UserRights(token)
+  if (is_user == false) {
+    return 401
+  }
+
+  // Check if user is banned
+  user, status := m.Userinfo_m(uid)
+  if (user.Is_banned == 1) {
+    return 403
+  }
+
+  // Generate a hash for unique comment id
+  hash := make([]byte, 64)
+  rand.Read(hash)
+  qid := u.MD5Hash(hash)
+
+  // Call the model man
+  status = m.CommentCreate_m(qid, aid, uid, content)
+  return status
+}
+
+func CommentFetch_c(token string, aid string) (coms []m.Comment, status int) {
+  // Retrieve uid to check if comment owned (for front purpose)
+  uid, admin := AdminRights(token)
+
+  // Retrieve all comments from the given post
+  coms, status = m.CommentFetch_m(uid, aid, admin)
+  return coms, status
+}
+
+func CommentDelete_c(token string, aid string, qid string) int {
+  // Check if user is logged, admin can delete anyone comments
+  uid, is_user := UserRights(token)
+  _, is_admin := AdminRights(token)
+  if (is_user == false) {
+    return 401
+  }
+
+  // Retrieve comment owner
+  owner, status := m.CommentGetOwner_m(qid)
+  if (status == 200 && owner != uid && is_admin == false) {
+    status = 401
+  }
+  if (status != 200) {
+    return status
+  }
+
+  // Delete the comment
+  status = m.CommentDelete_m(qid)
+  return status
+}
+
+func CommentUpdate_c(token string, qid string, content string) int {
+  // Check if user is logged
+  uid, is_user := UserRights(token)
+  if (is_user == false) {
+    return 401
+  }
+
+  // Check if user is banned
+  user, status := m.Userinfo_m(uid)
+  if (user.Is_banned == 1) {
+    return 403
+  }
+
+  // Retrieve comment owner
+  owner, status := m.CommentGetOwner_m(qid)
+  if (status == 200 && owner != uid) {
+    status = 401
+  }
+  if (status != 200) {
+    return status
+  }
+
+  // Update the comment
+  status = m.CommentUpdate_m(qid, content)
+  return status
+}
---
+++ server/models/comments.go
@@ -0,0 +1,80 @@
+package models
+
+import (
+  u "../utils"
+  "log"
+)
+
+type Comment struct {
+  Id string
+  Uid string
+  Aid string
+  Author string
+  Contents string
+  Creation_date string
+  Owned int
+}
+
+func CommentCreate_m(qid string, aid string, uid string, content string) int {
+  db = Db_m()
+  _, err := db.Exec("INSERT INTO comment(id, uid, aid, contents) VALUES ($1, $2, $3, $4);", qid, uid, aid, content)
+  return (u.CheckErr(err, 200))
+}
+
+func CommentDelete_m(qid string) int {
+  db = Db_m()
+  _, err := db.Exec("DELETE FROM comment WHERE id=$1;", qid)
+  return (u.CheckErr(err, 200))
+}
+
+func CommentUpdate_m(qid string, content string) int {
+  db = Db_m()
+  _, err := db.Exec("UPDATE comments SET contents=$2 WHERE id=$1;", qid, content)
+  return (u.CheckErr(err, 200))
+}
+
+func CommentFetch_m(uid string, aid string, admin bool) (coms []Comment, status int) {
+  db = Db_m()
+  var com Comment
+  rows, err := db.Query("SELECT c.id, c.uid, c.aid, c.contents, c.creation_date, u.username FROM comment c INNER JOIN articles a ON c.aid = a.id INNER JOIN users u ON c.uid = u.id WHERE c.aid=$1", aid)
+  if (err != nil) {
+    log.Println(err)
+    return coms, 500
+  }
+  defer rows.Close()
+  for rows.Next() {
+    err := rows.Scan(&com.Id, &com.Uid, &com.Aid, &com.Contents, &com.Creation_date, &com.Author)
+    // If admin and post not owned = Ban + Delete icons, if post owned = Delete icons, else no icon
+    if (admin == true && uid != com.Uid) {
+      com.Owned = 2
+    } else if (uid == com.Uid) {
+      com.Owned = 1
+    }
+    if (err != nil) {
+      log.Println(err)
+      return coms, 500
+    }
+    coms = append(coms, com)
+  }
+  return coms, 200
+}
+
+func CommentGetOwner_m(qid string) (owner string, status int) {
+  db = Db_m()
+  status = 404
+  rows, err := db.Query("SELECT uid FROM comments WHERE id=$1", qid)
+  if (err != nil) {
+    log.Println(err)
+    return owner, 500
+  }
+  defer rows.Close()
+  for rows.Next() {
+    err := rows.Scan(&owner)
+    if (err != nil) {
+      log.Println(err)
+      return owner, 500
+    }
+    status = 200
+  }
+  return owner, status
+}
